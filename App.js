6232c30415934688dcfd42d51ac247ebd3a7be50
Kevin Han <kevinhan@192-168-1-101.tpgi.com.au>
2020-04-14 21:30:54 +1000

    Buttons Added

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.topHalf}>
+        <Text style={styles.main}>hello</Text>
+      </View>
+      <View style={styles.bottomHalf}>
+        <Button
+          buttonStyle={styles.mainButtons}
+          title="Left button"
+          type="solid"
+          onPress={() => Alert.alert("Left button pressed")}
+        />
+        <Button
+          buttonStyle={styles.mainButtons}
+          title="Left button"
+          type="solid"
+          onPress={() => Alert.alert("Left button pressed")}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 2,
+    backgroundColor: "#fff",
+    textAlign: "center",
   },
+  topHalf: { flex: 1, backgroundColor: "red" },
+  bottomHalf: { flex: 1, marginTop: 10 },
+  main: { fontSize: 30, color: "black" },
+  mainButtons: { marginHorizontal: 80, marginVertical: 10 },
 });

2d305985fa0e7bf52293dfc148b8fe7bbd059e90
Kevin Han <kevinhan@192-168-1-101.tpgi.com.au>
2020-04-14 22:28:10 +1000

    Added Font and Logo

--- App.js
+++ App.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { StyleSheet, Text, View, Alert } from "react-native";
 import { Button } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { useFonts } from "@use-expo/font";
+
+function MainButtons() {
+  return (
+    <View>
+      <Button
+        buttonStyle={styles.mainButtons}
+        title="Left button"
+        color="red"
+        onPress={() => Alert.alert("Left button pressed")}
+      />
+      <Button
+        buttonStyle={styles.mainButtons}
+        title="Left button"
+        onPress={() => Alert.alert("Left button pressed")}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.topHalf}>
-        <Text style={styles.main}>hello</Text>
+        <Text style={styles.logo}>Study Jina Study</Text>
       </View>
       <View style={styles.bottomHalf}>
-        <Button
-          buttonStyle={styles.mainButtons}
-          title="Left button"
-          type="solid"
-          onPress={() => Alert.alert("Left button pressed")}
-        />
-        <Button
-          buttonStyle={styles.mainButtons}
-          title="Left button"
-          type="solid"
-          onPress={() => Alert.alert("Left button pressed")}
-        />
+        <MainButtons />
       </View>
     </View>
   );
@@ -30,11 +37,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 2,
-    backgroundColor: "#fff",
+    backgroundColor: "cornsilk",
     textAlign: "center",
   },
-  topHalf: { flex: 1, backgroundColor: "red" },
-  bottomHalf: { flex: 1, marginTop: 10 },
-  main: { fontSize: 30, color: "black" },
-  mainButtons: { marginHorizontal: 80, marginVertical: 10 },
+  topHalf: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bottomHalf: { flex: 1, marginTop: 10, borderStyle: "solid", borderWidth: 1 },
+  logo: { fontSize: 40, color: "black", fontWeight: "800" },
+  mainButtons: {
+    marginHorizontal: 80,
+    marginVertical: 10,
+  },
 });
